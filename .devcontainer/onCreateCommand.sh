49976617a6bfbdba7d64e2a1446bea03101d6c74
Wouter <w.vanden.broeke@student.vu.nl>
2025-12-13 21:38:12 +0100

    Update onCreateCommand.sh

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sdk use java 24-graal
+/workspace/fix-data-dir
 /usr/bin/python3 /data/baristabench/build.py
 
 sdk install java labsjdk-ce /data/jdk
@@ -11,6 +12,6 @@ mx -p /workspace/graal/substratevm intellijinit
 
 echo "export GRAALVM_HOME=$(sdk home java 24-graal)" >> ~/.bashrc
 find /workspace/graal -name mxbuild -print -type d -exec rm -rf {} \;
-/workspace/fix-data-dir
+
 
 exit 0
