bea5635581a6e29afa87793c739cea7932e3daa0
Wouter <w.vanden.broeke@student.vu.nl>
2025-12-13 20:38:03 +0100

    Reorder onCreateCommand.sh commands

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+sdk use java 24-graal
 /usr/bin/python3 /data/baristabench/build.py
-echo "export GRAALVM_HOME=$(sdk home java 24-graal)" >> ~/.bashrc
+
 sdk install java labsjdk-ce /data/jdk
+sdk use java labsjdk-ce
 sdk default java labsjdk-ce
-mx -p /workspace/graal/substratevm intellijinit
 mx -p /workspace/graal/substratevm build
+mx -p /workspace/graal/substratevm intellijinit
+
+echo "export GRAALVM_HOME=$(sdk home java 24-graal)" >> ~/.bashrc
 find /workspace/graal -name mxbuild -print -type d -exec rm -rf {} \;
 /workspace/fix-data-dir
 
