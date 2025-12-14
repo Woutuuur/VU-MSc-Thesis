5f4ae27500734c23a8206c36de1e4e99bf421b91
Wouter <w.vanden.broeke@student.vu.nl>
2025-12-14 02:35:13 +0100

    Reorder cleanup before build

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 sdk use java 24-graal
-/workspace/fix-data-dir
 /usr/bin/python3 /data/baristabench/build.py
 
 sdk install java labsjdk-ce /data/jdk
 sdk use java labsjdk-ce
 sdk default java labsjdk-ce
+
+# Clean up any existing mxbuild directories to create a clean devcontainer setup
+find /workspace/graal -name mxbuild -print -type d -exec rm -rf {} \;
+
 mx -p /workspace/graal/substratevm build
 mx -p /workspace/graal/substratevm intellijinit
 
 echo "export GRAALVM_HOME=$(sdk home java 24-graal)" >> ~/.bashrc
-find /workspace/graal -name mxbuild -print -type d -exec rm -rf {} \;
-
+/workspace/fix-data-dir
 
 exit 0
